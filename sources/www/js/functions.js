6301d8c3f4b917b373e32c86a707ecfc2de6f9a0
Thomas Deroua <thomasderoua@hotmail.com>
2017-04-27 15:11:20 +0200

    chose img + show img

---
+++ sources/www/js/functions.js
@@ -0,0 +1,29 @@
+ function setOptions(srcType) {
+      var options = {
+          // Some common settings are 20, 50, and 100
+          quality: 50,
+          destinationType: Camera.DestinationType.FILE_URI,
+          // In this app, dynamically set the picture source, Camera or photo gallery
+          sourceType: srcType,
+          encodingType: Camera.EncodingType.JPEG,
+          mediaType: Camera.MediaType.PICTURE,
+          allowEdit: true,
+          correctOrientation: true  //Corrects Android orientation quirks
+      }
+      return options;
+  }
+
+  function openFilePicker() {
+
+    var srcType = Camera.PictureSourceType.SAVEDPHOTOALBUM;
+    var options = setOptions(srcType);
+
+
+      navigator.camera.getPicture(function cameraSuccess(imageUri) {
+      document.getElementById("send-img").innerHTML = "<h3>Ma photo :</h3><img src='"+imageUri+"' width='100%' />";
+      document.getElementById("close-img").style.display = "block";
+    }, function cameraError(error) {
+     console.log("Une erreur s'est produite.");
+     console.log(error);
+    }, options);
+  }
