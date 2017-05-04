2ae7c26143b8ab99780fd8099d3cca88557acf37
Mélissa <melissa.loeser@gmail.com>
2017-05-04 15:05:08 +0200

    add function sendValueGeoloc less jquerry

--- sources/www/js/functions.js
+++ sources/www/js/functions.js
@@ -28,3 +28,84 @@
     }, options);
   }
 
+
+
+function onSuccessGeo(position) {
+
+      $.get( "http://maps.googleapis.com/maps/api/geocode/json?latlng="+position.coords.latitude+","+position.coords.longitude, function( data ) {
+            console.log(data);
+            console.log(data.results[6].address_components[0].long_name);
+
+            var dataRegion = data.results[6].address_components[0].long_name;
+            console.log(dataRegion);
+            var region = document.getElementById('region');
+            var title = document.getElementById('title');
+            var description = document.getElementById('description');
+
+            switch (dataRegion) {
+              case "Île-de-France":
+
+                title.innerHTML = 'Plats' + title.innerHTML;
+                description.innerHTML = '<img src="http://www.toutlevin.com/uploads/dish/langouste-mayonnaise-big.jpg" alt="langouste à la parisienne" width="300px"/>' + description.innerHTML;
+                region.innerHTML = dataRegion;
+              break;
+              case "Bretagne":
+
+               title.innerHTML = 'Plats' + title.innerHTML;
+               description.innerHTML = '<img src="https://kiwings-images-prod.s3-eu-west-1.amazonaws.com/recipes/527a206fb1045.jpeg" alt="langouste à la parisienne" width="300px"/>' + description.innerHTML;
+               region.innerHTML = dataRegion;
+              break;
+              case "Provence-Alpes-Côte d'Azur":
+
+               title.innerHTML = 'Plats' + title.innerHTML;
+               description.innerHTML = '<img src="http://www.bestcharmingbnb.com/tables-hote/gastronomie-provence-alpes-cote-dazur/legumes-grilles-gastronomie-provence-alpes-cote-d-azur-4.jpg" alt="langouste à la parisienne" width="300px"/>' + description.innerHTML;
+               region.innerHTML = dataRegion;
+              break;
+              default:
+                 description.innerHTML = 'Aucune thématique est disponible pour cette région' + description.innerHTML;
+                 region.innerHTML = dataRegion;
+             }
+
+             console.log(position.coords.latitude,position.coords.longitude,position.coords.accuracy,position.coords.altitude);
+
+      });
+  }
+
+function sendValueGeoloc(){
+
+  var latitudeGeoloc = document.getElementById('latitudeGeoloc').value;
+  var longitudeGeoloc = document.getElementById('longitudeGeoloc').value;
+
+  var getJSON = function(url, successHandler, errorHandler) {
+    var xhr = typeof XMLHttpRequest != 'undefined'
+      ? new XMLHttpRequest()
+      : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = function() {
+      var status;
+      var data;
+      // https://xhr.spec.whatwg.org/#dom-xmlhttprequest-readystate
+      if (xhr.readyState == 4) { // `DONE`
+        status = xhr.status;
+        if (status == 200) {
+          successHandler && successHandler(xhr.response);
+        } else {
+          errorHandler && errorHandler(status);
+        }
+      }
+    };
+    xhr.send();
+  };
+
+  getJSON("http://maps.googleapis.com/maps/api/geocode/json?latlng="+latitudeGeoloc+","+longitudeGeoloc, function(data) {
+  var dataRegion = data.results[0].address_components[4].long_name;
+                console.log(dataRegion);
+    document.getElementById("newregion").innerHTML = dataRegion;
+            console.log(latitudeGeoloc, longitudeGeoloc);
+  }, function(status) {
+    document.getElementById("newregion").innerHTML = dataRegion;
+            console.log(latitudeGeoloc, longitudeGeoloc);
+  });
+
+}
